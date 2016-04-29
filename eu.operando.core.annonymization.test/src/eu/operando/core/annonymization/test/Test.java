5b828fcc6c89e708946ec2c2f18269513c8221d8
gechevarria <gorka.echevarria@tecnalia.com>
2016-04-29 09:02:32 +0200

    Added MIT license related header

--- eu.operando.core.annonymization.test/src/eu/operando/core/annonymization/test/Test.java
+++ eu.operando.core.annonymization.test/src/eu/operando/core/annonymization/test/Test.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ *  * Copyright (c) 2016 {TECNALIA}.
+ *  * All rights reserved. This program and the accompanying materials
+ *  * are made available under the terms of the The MIT License (MIT).
+ *  * which accompanies this distribution, and is available at
+ *  * http://opensource.org/licenses/MIT
+ *  *
+ *  * Contributors:
+ *  *    Gorka Mikel Echevarría {TECNALIA}
+ *  * Initially developed in the context of OPERANDO EU project www.operando.eu
+ *******************************************************************************/
 package eu.operando.core.annonymization.test;
 
 import java.util.ArrayList;
--- eu.operando.core.anonymization.server/src/gen/java/io/swagger/api/ApiException.java
+++ eu.operando.core.anonymization.server/src/gen/java/io/swagger/api/ApiException.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ *  * Copyright (c) 2016 {TECNALIA}.
+ *  * All rights reserved. This program and the accompanying materials
+ *  * are made available under the terms of the The MIT License (MIT).
+ *  * which accompanies this distribution, and is available at
+ *  * http://opensource.org/licenses/MIT
+ *  *
+ *  * Contributors:
+ *  *    Gorka Mikel Echevarría {TECNALIA}
+ *  * Initially developed in the context of OPERANDO EU project www.operando.eu
+ *******************************************************************************/
 package io.swagger.api;
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-03-16T08:19:16.233Z")
